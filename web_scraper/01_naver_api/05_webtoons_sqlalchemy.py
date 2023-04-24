0e7c3fd337aae6305862463ffcba245baa7cf8e8
nob0dj <dongxuan1863@gmail.com>
2023-04-24 20:19:19 +0900

    web_scraper sqlalchemy 연결. WebtoonRank 클래스 생성

--- web_scraper/01_naver_api/05_webtoons_sqlalchemy.py
+++ web_scraper/01_naver_api/05_webtoons_sqlalchemy.py
@@ -1,26 +1,26 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float
+from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import datetime
 
-engine = create_engine('sqlite:///example.db', echo=True)
+engine = create_engine('sqlite:///naver_api.db', echo=True)
 
 Base = declarative_base()
 
 
-class WebtoonsRank(Base):
-    __tablename__ = 'webtoons_rank'
-
+class WebtoonRank(Base):
+    __tablename__ = 'webtoon_rank'
     id = Column(Integer, primary_key=True, autoincrement=True)
     rank = Column(Integer)
+    star = Column(Float)
     title = Column(String)
     created_at = Column(DateTime, default=datetime.now())
 
-    def __init__(self, rank, title):
+    def __init__(self, rank, star, title):
         self.rank = rank
+        self.star = star
         self.title = title
-        self.created_at = Column(DateTime, default=datetime.now())
-
+    def __repr__(self):
+        return f"WebtoonRank(id={self.id!r}, rank={self.rank!r}, star={self.star!r}, title={self.title!r}, created_at={self.created_at!r})"
 
 Base.metadata.create_all(engine)
 
@@ -29,6 +29,9 @@ session = Session()
 
 rank = 1
 title = '짱구는 못말려'
-new_rank = WebtoonsRank(rank, title)
+star = 9.8
+new_rank = WebtoonRank(rank, star, title)
 session.add(new_rank)
 session.commit()
+
+print(new_rank)
